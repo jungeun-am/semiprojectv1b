7ac6cda0a1909bd758b1594a644af2cf40356cd1
jungeun <jungeun0054@gmail.com>
2025-03-05 12:49:45 +0900

    게시판 댓글쓰기 구현 3 - 댓글과 대댓글 목록 (구별) 구현

--- src/main/java/com/example/zzyzzy/semiprojectv1/service/BoardServiceImpl.java
+++ src/main/java/com/example/zzyzzy/semiprojectv1/service/BoardServiceImpl.java
@@ -80,5 +80,11 @@ public class BoardServiceImpl implements BoardService {
         return boardMapper.selectReply(pno);
     }
 
+    @Override
+    public boolean newComment(NewReplyDTO newReplyDTO) {
+        int result = boardMapper.insertComment(newReplyDTO);
+        return result > 0;
+    }
+
 
 }
