eb24f6d4343b9eac440709dde68b4a241ce9d84b
jungeun <jungeun0054@gmail.com>
2025-03-06 12:45:17 +0900

    갤러리 구현 2 - Gallery 클래스 정의 GalleryService, GalleryRepository 구현

---
+++ src/main/java/com/example/zzyzzy/semiprojectv1/repository/GalleryRepository.java
@@ -0,0 +1,17 @@
+package com.example.zzyzzy.semiprojectv1.repository;
+
+
+import com.example.zzyzzy.semiprojectv1.domain.Gallery;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+@Mapper
+public interface GalleryRepository {
+
+    @Select("select gno, title, userid, simgname, regdate from gallerys order by gno desc ")
+    List<Gallery> selectGallery();
+
+}
+
