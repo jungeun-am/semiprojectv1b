68537e62cfe92a548656a12e7850fead9ee33ba3
jungeun <jungeun0054@gmail.com>
2025-03-07 11:21:46 +0900

    갤러리 구현 4 - 갤러리 본문 출력 구현 본문글 URL 지정은 경로매개변수 사용 본문글, 첨부이미지를 위한 dto 정의

--- src/main/java/com/example/zzyzzy/semiprojectv1/repository/GalleryRepository.java
+++ src/main/java/com/example/zzyzzy/semiprojectv1/repository/GalleryRepository.java
@@ -1,9 +1,13 @@
 package com.example.zzyzzy.semiprojectv1.repository;
 
 
-import com.example.zzyzzy.semiprojectv1.domain.Gallery;
+import com.example.zzyzzy.semiprojectv1.domain.GalleryImage;
+import com.example.zzyzzy.semiprojectv1.domain.GalleryImageDTO;
+import com.example.zzyzzy.semiprojectv1.domain.GalleryListDTO;
+import com.example.zzyzzy.semiprojectv1.domain.GalleryViewDTO;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import java.util.List;
 
@@ -11,7 +15,15 @@ import java.util.List;
 public interface GalleryRepository {
 
     @Select("select gno, title, userid,regdate, thumbs, views, simgname from gallerys order by gno desc ")
-    List<Gallery> selectGallery();
+    List<GalleryListDTO> selectGallery();
 
+    @Update("update gallerys set views = views + 1 where gno = #{gno}")
+    void updateViewOne(int gno);
+
+    @Select("select gno, title, userid,regdate, thumbs, views, contents from gallerys where gno = #{gno}")
+    GalleryViewDTO selectOneGallery(int gno);
+
+    @Select("select imgname, imgsize from gallery_images where gno = #{gno}")
+    List<GalleryImage> selectGalleryImages(int gno);
 }
 
