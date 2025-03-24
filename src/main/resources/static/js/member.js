077c1ec7726f5a241bb69210d0bed447870fcc28
jungeun <jungeun0054@gmail.com>
2025-03-24 12:41:41 +0900

    로그인 후 게시판 리스트에 새글쓰기 버튼, 로그인 후 새글쓰기에 작성자 아이디 표시

--- src/main/resources/static/js/member.js
+++ src/main/resources/static/js/member.js
@@ -88,8 +88,8 @@ const hashPassword = async (passwd) => {
 // await : 비동기 함수의 처리 완료될때까지 기다렸다가 결과를 받아옴
 const submitJoinfrm = async (frm) => {
     // Web Crypto API로 비밀번호 암호화
-    // frm.passwd.value = await hashPassword(frm.passwd.value);
-    // console.log(frm.passwd.value);
+    //frm.passwd.value = await hashPassword(frm.passwd.value);
+    //console.log(frm.passwd.value);
 
     // 폼에 입력된 데이터를 formData 객체로 초기화
     const formData = new FormData(frm);
@@ -101,7 +101,7 @@ const submitJoinfrm = async (frm) => {
         if (response.ok) { // 회원가입이 정상적으로 처리되었다면
             alert('회원가입이 완료되었습니다!!');
             location.href = '/member/login';
-        } else if (response.status === 400) {
+        } else if (response.status === 401) {
             alert(await response.text());
         } else { // 회원가입이 실패했다면
             alert('회원가입에 실패했습니다!! 다시 시도해 주세요!');
@@ -130,7 +130,7 @@ const validLogin = (form) => {
 
 // 로그인 폼 제출
 const submitLoginfrm = async (frm) => {
-  //  frm.passwd.value = await hashPassword(frm.passwd.value);
+    //frm.passwd.value = await hashPassword(frm.passwd.value);
     const formData = new FormData(frm);
 
     fetch('/member/login', {
