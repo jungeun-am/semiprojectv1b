ec20ab1b6d2e3cb5591f53dd4133d75f509ebf09
jungeun <jungeun0054@gmail.com>
2025-03-24 10:07:25 +0900

    시큐리티 폼인증을 위해 MemberService 수정

--- src/main/java/com/example/zzyzzy/semiprojectv1/service/MemberService.java
+++ src/main/java/com/example/zzyzzy/semiprojectv1/service/MemberService.java
@@ -1,9 +1,12 @@
 package com.example.zzyzzy.semiprojectv1.service;
 
-import com.example.zzyzzy.semiprojectv1.domain.Member;
 import com.example.zzyzzy.semiprojectv1.domain.MemberDTO;
+import com.example.zzyzzy.semiprojectv1.domain.User;
 import com.example.zzyzzy.semiprojectv1.repository.MemberRepository;
+import com.example.zzyzzy.semiprojectv1.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,9 +14,10 @@ import org.springframework.stereotype.Service;
 public class MemberService {
 
     private final MemberRepository memberMapper;
+    private final UserRepository userRepository;
 
     public boolean newMember(MemberDTO member) {
-        
+
         // 아이디 중복 체크
         if (memberMapper.countByUserid(member.getUserid()) > 0) {
             throw new IllegalStateException("이미 존재하는 아이디입니다!!");
@@ -23,20 +27,28 @@ public class MemberService {
         if (memberMapper.countByEmail(member.getEmail()) > 0) {
             throw new IllegalStateException("이미 존재하는 이메일입니다!!");
         }
-                
+
         int result = memberMapper.insertMember(member);
         return result == 1;  // 회원정보가 테이블 저장되었는지 여부에 따라
-                             // true/false 반환
+        // true/false 반환
     }
 
-    public Member loginMember(MemberDTO member) {
-        Member findMember = memberMapper.findByUserid(member.getUserid());
+    // 스프링 시큐리티가 자동으로 처리 - 생략
+//    public Member loginMember(MemberDTO member) {
+//        Member findMember = memberMapper.findByUserid(member.getUserid());
+//
+//        if (findMember == null || !findMember.getPasswd().equals(member.getPasswd())) {
+//            throw new IllegalStateException("아이디나 비밀번호가 일치하지 않습니다!!");
+//        }
+//
+//        return findMember;
+//    }
 
-        if (findMember == null || !findMember.getPasswd().equals(member.getPasswd())) {
-            throw new IllegalStateException("아이디나 비밀번호가 일치하지 않습니다!!");
-        }
+    public User findByUserid(UserDetails userDetails) {
+        User findUser = userRepository.findByUserid(userDetails.getUsername())
+                .orElseThrow(() -> new UsernameNotFoundException("사용자가 존재하지 않습니다!!"));
 
-        return findMember;
+        return findUser;
     }
 
 }
